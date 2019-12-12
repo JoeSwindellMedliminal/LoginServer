8f39ed5e9dbd4b34b030cf1e9a668f54226413e1
MRSLLC\joes <joes@medliminal.com>
2019-12-12 16:28:24 -0500

    Added Class Library. Pretty console colors that will eventually split off into its own library

---
+++ LoginServer/LoginServerClassLibrary/ServerConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginServerClassLibrary
+{
+    public class ServerConfig
+    {
+        private string username;
+        private string password;
+        private string database;
+        private string serverAddress;
+        private string port;
+
+        public string ConnectionString;
+
+        public ServerConfig(string UserName, string Password, string Databse, string ServerAddress, string Port)
+        {
+            username = UserName;
+            password = Password;
+            database = Databse;
+            serverAddress = ServerAddress;
+            port = Port;
+
+            CreateMySQLConnectionString();
+
+        }
+
+        
+        public void CreateMySQLConnectionString()
+        {
+            ConnectionString = String.Format("server={0};user={1};database={2};port={3};password={4}", serverAddress, username, database, port, password);
+        }
+    }
+}
--- LoginServer/LoginServerConsole/Program.cs
+++ LoginServer/LoginServerConsole/Program.cs
@@ -3,13 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using System.IO;
 
 namespace LoginServerConsole
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        const string STAR_WARNING = "";
+        public static void Main()
         {
+            bool serverConfigLoad = LoadServerConfig();
+          
+            if (serverConfigLoad)
+            {
+                WriteLineColoredMessage("server.config loaded", ConsoleColor.White, ConsoleColor.Blue);
+            } else
+            {
+                WriteLineColoredMessage("server.config not loaded", ConsoleColor.Yellow, ConsoleColor.Red);
+            }
+            Console.ReadKey();
         }
+
+
+        public static bool LoadServerConfig()
+        {
+
+            // server.config is missing
+            if (!File.Exists("server.configa"))
+            {
+                WriteStarError("server.config is missing");                
+                return false;
+            }
+
+
+            StreamReader reader = File.OpenText("server.config");
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+
+       
+
+            return true;
+        }
+
+
+        public static void WriteStarError(string msgToWrite)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("********************************************************************************");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msgToWrite);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("********************************************************************************");
+            Console.ResetColor();
+        }
+
+        public static void WriteLineColoredMessage(String msg, ConsoleColor foreGround, ConsoleColor backGround = ConsoleColor.Black)
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = foreGround;
+            Console.BackgroundColor = backGround;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+
     }
 }
